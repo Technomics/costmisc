0b04dc21dd9675754217a8247d49bf746562367d
Adam James <ajames@technomics.net>
2020-05-12 12:26:32 -0400

    Closes #2

--- R/list_functions.R
+++ R/list_functions.R
@@ -1,69 +1,51 @@
 ## ===== List Functions =====
 
-#' Internal function to support stacking FlexFiles
+#' Unnest identical data frames
 #'
-#' @description
-#'
-#' \code{unnest_df()} supports FlexFile stacking Internal function.
+#' \code{unnest_df()} stacks a list of lists of dataframes. This assumes that each
+#' level 2 list has the same dataframes with the same columns. This is used when
+#' reading in a set of files with identical format.
 #'
 #' @export
 #'
-#' @param .data A list of FlexFile submissions' tibbles converted from JSON format.
+#' @param .data A list of lists of dataframes.
 #'
-#' @return A list of stacked tibbles of multiple dataframes
+#' @return A list of stacked tibbles.
+#' @seealso \code{\link{listindex_to_col}}
+#'
 unnest_df <- function(.data) {
   purrr::flatten(.data) %>%
     split(., names(.)) %>%
     purrr::map(dplyr::bind_rows)
 }
 
-#' Add id column to a list
-#'
-#' @description
-#'
-#' \code{listindex_to_col()} adds the listindex from the first list of a list of data frames
-#' as an ID column in each of the nested data frames.
-#'
-#' @export
-#'
-#'
-#' @param .nestedlist A nested list of data frames.
-#'
-#' @examples
-#' files <- system.file("extdata/multiple-flexfiles", package = "csdrtools")
-#'
-#' flexfiles <- read_folder(files, read_ff) %>%
-#' listindex_to_col()
-#'
-
-listindex_to_col <- function(.nestedlist) {
-  purrr::map(seq_along(.nestedlist), ~ add_id_col(.nestedlist[[.]], ., "ff_id"))
-}
-
 #' Add id column to dataframe
-#'
-#' @description
 #'
 #' \code{add_id_col()} adds an id column to either a data frame or to each data frame in a list.
 #'
 #' @export
 #'
-#'
-#' @param .data A list of FlexFile submissions' tibbles converted from JSON format.
+#' @param .data A list (or list of lists) of data frames.
 #' @param value A value to assign as the id.
-#' @param var Character name of which to name the id column.
+#' @param var Character name of the new id column.
 #'
-#' @examples
-#' file <- system.file("extdata", "Sample File_FF.zip", package = "csdrtools")
-#'
-#' flexfile <- read_ff(file) %>%
-#' add_id_col()
-#'
-
-add_id_col <- function(.data, value = "1", var = "ff_id") {
+add_id_col <- function(.data, value = "1", var = "doc_id") {
   if (class(.data) == "data.frame") {
     tibble::add_column(.data, !!var := value, .before = 1)
   } else if (class(.data) == "list") {
     purrr::map(.data, ~ tibble::add_column(., !!var := value, .before = 1))
   }
+}
+
+#' Add id columns to dataframes
+#'
+#' \code{listindex_to_col()} adds the listindex from the first list of a list of data frames
+#' as an ID column in each of the nested data frames.
+#'
+#' @export
+#' @rdname add_id_col
+#' @seealso \code{\link{unnest_df}}
+#'
+listindex_to_col <- function(.data, var = "doc_id") {
+  purrr::map(seq_along(.data), ~ add_id_col(.data[[.]], ., var))
 }
