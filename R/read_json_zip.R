416a56b6c4f55fc259fbe898aa920f6dccfbc1ce
Adam H. James <ajames@technomics.net>
2021-02-06 11:17:49 -0500

    Corrected bug in read_json_zip()

--- R/read_json_zip.R
+++ R/read_json_zip.R
@@ -13,10 +13,10 @@ read_json_zip <- function(path, .warn_utf8_bom = TRUE) {
 
   # process contents of the zip folder
   json_files_all <- dplyr::transmute(zip_contents,
-                                 dir = fs::path_dir(.data$filename),
-                                 file = fs::path_file(.data$filename),
-                                 ext = fs::path_ext(.data$filename),
-                                 basename = fs::path_ext_remove(.data$file))
+                                     dir = fs::path_dir(.data$filename),
+                                     file = fs::path_file(.data$filename),
+                                     ext = fs::path_ext(.data$filename),
+                                     basename = fs::path_ext_remove(.data$file))
 
   json_files <- dplyr::filter(json_files_all,
                               .data$dir == ".", .data$ext == "json")
@@ -28,29 +28,20 @@ read_json_zip <- function(path, .warn_utf8_bom = TRUE) {
   read_zip_file <- function(zipped_filename) {
 
     # collect UTF8 BOM errors for later
-    json_list <- withCallingHandlers(jsonlite::parse_json(unz(path, zipped_filename)),
-                                     warning = function(w) {
-                                       if (grepl("UTF8 byte-order-mark!", w$message)) {
-                                         utf_warnings[zipped_filename] <<- TRUE
-                                         invokeRestart("muffleWarning")
-                                       } else {
-                                         message(w$message)
-                                       }
-                                     }
+    json_input <- withCallingHandlers(
+      jsonlite::parse_json(unz(path, zipped_filename), simplifyVector = TRUE),
+      warning = function(w) {
+        if (grepl("UTF8 byte-order-mark!", w$message)) {
+          utf_warnings[zipped_filename] <<- TRUE
+          invokeRestart("muffleWarning")
+        } else {
+          message(w$message)
+        }
+      }
     )
 
-    # handle a list of rows or a single row
-    f_flatten <- function(x) {
-      if (is.list(x)) {
-        purrr::flatten(x)
-      } else {
-        if (is.null(x)) NA else x
-      }
-
-    }
-
-    # flatten the list to a dataframe
-    purrr::map_dfr(json_list, f_flatten)
+    # if a list (i.e., could not simplify), then must replace NULLs
+    fix_json_null(json_input)
   }
 
   lst_json <- purrr::map(json_to_read, read_zip_file)
@@ -62,3 +53,29 @@ read_json_zip <- function(path, .warn_utf8_bom = TRUE) {
   lst_json
 
 }
+
+#' Fix 'null' values in JSON
+#'
+#' \code{fix_json_null()} replaces 'null' values in the JSON with an NA. This
+#' is needed for single row tables in the JSON with a null value. In this case,
+#' \code{\link[jsonlite]{read_json}(..., simplifyVector = FALSE)} returns a list instead
+#' of a data.frame because of the 'null'. This function takes that list, replaces the 'null'
+#' with an \code{NA} and returns the data.frame.
+#'
+#' @param json_object An object read in from \code{read_json()}.
+#'
+#' @return A corrected tibble with \code{NA} in place of the null.
+#' @export
+#'
+fix_json_null <- function(json_object) {
+
+  if (!is.data.frame(json_object)) {
+    purrr::map_dfc(json_object, fix_null)
+  } else {
+    dplyr::as_tibble(json_object)
+  }
+
+}
+
+#' @keywords internal
+fix_null <- function(x) if (is.null(x)) NA else x
